0bfedeb9668b956179f582d20415804473921e01
miraheka2305 <miraheka@outlook.com>
2018-08-07 17:18:55 +0700

    [Ref T5320] Add functions to set height and hide/show expand button

--- src/compoz.test.ts
+++ src/compoz.test.ts
@@ -2,124 +2,137 @@
 // Use of this source code is governed by a MIT-style license that can be found
 // in the LICENSE file.
 
-import {Compoz} from './compoz';
-import {CompozFile, CompozFileInterface} from './compozfile';
-import {FileState} from './filestate';
+import { Compoz } from "./compoz";
+import { CompozFile, CompozFileInterface } from "./compozfile";
+import { FileState } from "./filestate";
 
 function generateContent() {
-  const elContent = document.querySelector('div.panel div.content');
+	const elContent = document.querySelector("div.panel div.content");
 
-  for (let x = 0; x < 100; x++) {
-    const d = document.createElement('div');
-    d.innerText = 'Test ' + x;
+	for (let x = 0; x < 100; x++) {
+		const d = document.createElement("div");
+		d.innerText = "Test " + x;
 
-    if (elContent) {
-      elContent.appendChild(d);
-    }
-  }
+		if (elContent) {
+			elContent.appendChild(d);
+		}
+	}
 }
 
 function createQuickCompose() {
-  const config = {
-    contentHTML: '<b> this is </b> the content',
-    fileMaxSize: 4194304,
+	const config = {
+		subject: "Bootcamp LPDP",
+		participants: "You, Kukuh, Mirah, Matias & Mas Shulhan",
+		avatarURL:
+			"https://d26i9nvlq0mz5u.cloudfront.net/avatar/default/v0/avatar192.png",
+		contentHTML: "<b> this is </b> the content",
+		fileMaxSize: 4194304,
 
-    height: 0,
-    hideAttachment: false,
-    hideDiscard: true,
-    hideExpand: false,
-    hideSave: false,
-    hideSend: false,
+		height: 0,
+		hideAttachment: false,
+		hideDiscard: true,
+		hideExpand: false,
+		hideSave: false,
+		hideSend: false,
 
-    onDiscard: () => {
-      console.log('onDiscard');
-    },
-    onSave: () => {
-      console.log('onSave');
-    },
-    onSend: () => {
-      console.log('content:', c.getContentHTML());
+		onDiscard: () => {
+			console.log("onDiscard");
+		},
+		onSave: () => {
+			console.log("onSave");
+		},
+		onSend: () => {
+			console.log("content:", c.getContentHTML());
 
-      const files = c.getFiles();
+			const files = c.getFiles();
 
-      for (let x = 0; x < files.length; x++) {
-        files[x].setState(FileState.UPLOADING);
-      }
+			for (let x = 0; x < files.length; x++) {
+				files[x].setState(FileState.UPLOADING);
+			}
 
-      setTimeout(() => {
-        for (let x = 0; x < files.length; x++) {
-          if ((x % 2) === 1) {
-            files[x].setState(FileState.SUCCESS);
-          } else {
-            files[x].setState(FileState.ERROR);
-          }
-        }
-      }, 2000);
-    },
-    onBlur: null,
-    onContentChange: null,
-    onExpand: () => {
-      console.log('onExpand');
-    },
-    onFileDeletedBefore: (f: CompozFile) => {
-      console.log('onFileDeletedBefore:', f);
-      return Promise.resolve(true);
-    }
-  };
+			setTimeout(() => {
+				for (let x = 0; x < files.length; x++) {
+					if (x % 2 === 1) {
+						files[x].setState(FileState.SUCCESS);
+					} else {
+						files[x].setState(FileState.ERROR);
+					}
+				}
+			}, 2000);
+		},
+		onBlur: null,
+		onContentChange: null,
+		onExpand: () => {
+			c.setHeight(400);
+			c.hideButtonExpand();
+			setTimeout(() => {
+				c.showButtonExpand();
+				console.log("show");
+			}, 2000);
+		},
+		onFileDeletedBefore: (f: CompozFile) => {
+			console.log("onFileDeletedBefore:", f);
+			return Promise.resolve(true);
+		}
+	};
 
-  const c = new Compoz('quick-compose', config, []);
+	const c = new Compoz("quick-compose", config, []);
 }
 
 function createFullCompose() {
-  const files: CompozFileInterface[] = [];
+	const files: CompozFileInterface[] = [];
 
-  files.push({
-    raw: null,
-    id: 2,
-    name: 'test.jpg',
-    size: 512,
-    type: 'image/png',
-  });
+	files.push({
+		raw: null,
+		id: 2,
+		name: "test.jpg",
+		size: 512,
+		type: "image/png"
+	});
 
-  const config = {
-    contentHTML: '',
-    height: 500,
-    fileMaxSize: 0,
+	const config = {
+		subject: "Bootcamp LPDP",
+		participants: "You, Kukuh, Mirah, Matias & Mas Shulhan",
+		avatarURL:
+			"https://d26i9nvlq0mz5u.cloudfront.net/avatar/default/v0/avatar192.png",
+		contentHTML: "",
+		height: 500,
+		fileMaxSize: 0,
 
-    hideAttachment: true,
-    hideDiscard: false,
-    hideExpand: true,
-    hideSave: true,
-    hideSend: false,
+		hideAttachment: true,
+		hideDiscard: false,
+		hideExpand: true,
+		hideSave: true,
+		hideSend: false,
 
-    onDiscard: () => {
-      console.log('onDiscard');
-    },
-    onSave: () => {
-      console.log('onSave');
-    },
-    onSend: () => {
-      console.log('content:', c.getContentHTML());
-      console.log('files:', c.getFiles());
-    },
-    onExpand: () => {
-      console.log('onExpand');
-    },
-    onContentChange: (contentHTML: string) => {
-      console.log('onContentChange:', contentHTML);
-    },
-    onBlur: () => {
-      console.log('onBlur');
-    },
-    onFileDeletedBefore: (f: CompozFile): Promise<boolean> => {
-      console.log('onFileDeletedBefore:', f);
-      return Promise.resolve(false);
-    }
-  };
+		onDiscard: () => {
+			console.log("onDiscard");
+		},
+		onSave: () => {
+			console.log("onSave");
+		},
+		onSend: () => {
+			console.log("content:", c.getContentHTML());
+			console.log("files:", c.getFiles());
+		},
+		onExpand: () => {
+			console.log("onExpand");
+		},
+		onContentChange: (contentHTML: string) => {
+			console.log("onContentChange:", contentHTML);
+		},
+		onBlur: () => {
+			console.log("onBlur");
+		},
+		onFileDeletedBefore: (f: CompozFile): Promise<boolean> => {
+			console.log("onFileDeletedBefore:", f);
+			return Promise.resolve(false);
+		}
+	};
 
-  const c = new Compoz('full-compose', config, files);
+	const c = new Compoz("full-compose", config, files);
 
-  c.setContentHTML('<b>Test</b> set content');
+	c.setContentHTML("<b>Test</b> set content");
 }
 
 generateContent();
--- src/config.ts
+++ src/config.ts
@@ -2,65 +2,71 @@
  * ConfigInterface define an object to configure the compose component.
  */
 export interface ConfigInterface {
-  contentHTML: string;
-  fileMaxSize: number;
-  height: number;
-  hideAttachment: boolean;
-  hideDiscard: boolean;
-  hideExpand: boolean;
-  hideSave: boolean;
-  hideSend: boolean;
-  onBlur: Function|null;
-  onContentChange: Function|null;
-  onFileDeletedBefore: Function|null;
-  onExpand: Function|null;
-  onDiscard: Function|null;
-  onSave: Function|null;
-  onSend: Function|null;
+	subject: string;
+	participants: string;
+	avatarURL: string;
+	contentHTML: string;
+	fileMaxSize: number;
+	height: number;
+	hideAttachment: boolean;
+	hideDiscard: boolean;
+	hideExpand: boolean;
+	hideSave: boolean;
+	hideSend: boolean;
+	onBlur: Function | null;
+	onContentChange: Function | null;
+	onFileDeletedBefore: Function | null;
+	onExpand: Function | null;
+	onDiscard: Function | null;
+	onSave: Function | null;
+	onSend: Function | null;
 }
 
 export class Config {
-  contentHTML = '';
-  fileMaxSize = 26214400;
-  height = 0;
-  hideAttachment = false;
-  hideDiscard = false;
-  hideExpand = false;
-  hideSave = false;
-  hideSend = false;
-  onBlur: Function|null = null;
-  onContentChange: Function|null = null;
-  onExpand: Function|null = null;
-  onFileDeletedBefore: Function|null = null;
-  onDiscard: Function|null = null;
-  onSave: Function|null = null;
-  onSend: Function|null = null;
+	subject = "";
+	participants = "";
+	avatarURL = "";
+	contentHTML = "";
+	fileMaxSize = 26214400;
+	height = 0;
+	hideAttachment = false;
+	hideDiscard = false;
+	hideExpand = false;
+	hideSave = false;
+	hideSend = false;
+	onBlur: Function | null = null;
+	onContentChange: Function | null = null;
+	onExpand: Function | null = null;
+	onFileDeletedBefore: Function | null = null;
+	onDiscard: Function | null = null;
+	onSave: Function | null = null;
+	onSend: Function | null = null;
 
-  constructor(cfg: ConfigInterface|null) {
-    if (!cfg) {
-      return;
-    }
+	constructor(cfg: ConfigInterface | null) {
+		if (!cfg) {
+			return;
+		}
 
-    this.contentHTML = cfg.contentHTML ? cfg.contentHTML : '';
+		this.contentHTML = cfg.contentHTML ? cfg.contentHTML : "";
 
-    if (cfg.fileMaxSize && cfg.fileMaxSize > 0) {
-      this.fileMaxSize = cfg.fileMaxSize;
-    }
-    if (cfg.height && cfg.height > 0) {
-      this.height = cfg.height;
-    }
+		if (cfg.fileMaxSize && cfg.fileMaxSize > 0) {
+			this.fileMaxSize = cfg.fileMaxSize;
+		}
+		if (cfg.height && cfg.height > 0) {
+			this.height = cfg.height;
+		}
 
-    this.hideAttachment = cfg.hideAttachment;
-    this.hideDiscard = cfg.hideDiscard;
-    this.hideExpand = cfg.hideExpand;
-    this.hideSave = cfg.hideSave;
-    this.hideSend = cfg.hideSend;
-    this.onBlur = cfg.onBlur;
-    this.onContentChange = cfg.onContentChange;
-    this.onExpand = cfg.onExpand;
-    this.onFileDeletedBefore = cfg.onFileDeletedBefore;
-    this.onDiscard = cfg.onDiscard;
-    this.onSave = cfg.onSave;
-    this.onSend = cfg.onSend;
-  }
+		this.hideAttachment = cfg.hideAttachment;
+		this.hideDiscard = cfg.hideDiscard;
+		this.hideExpand = cfg.hideExpand;
+		this.hideSave = cfg.hideSave;
+		this.hideSend = cfg.hideSend;
+		this.onBlur = cfg.onBlur;
+		this.onContentChange = cfg.onContentChange;
+		this.onExpand = cfg.onExpand;
+		this.onFileDeletedBefore = cfg.onFileDeletedBefore;
+		this.onDiscard = cfg.onDiscard;
+		this.onSave = cfg.onSave;
+		this.onSend = cfg.onSend;
+	}
 }
