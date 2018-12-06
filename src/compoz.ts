a0d65bea4eaa1d3b4be0c880853cf1f88707f011
shulhan <shulhan@proofn.com>
2018-12-06 15:41:42 +0700

    Add polyfill for es6 promise

--- src/compoz.ts
+++ src/compoz.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT-style license that can be found
 // in the LICENSE file.
 
+import "es6-promise"
 import "./compoz.scss"
 
 import { CompozFile, ICompozFile } from "./compozfile"
